67347463afa164f81f66de0341de02a5ae91c56e
Nathan <diicky@gmail.com>
2019-10-13 19:49:18 -0700

    Initial Commit

---
+++ Sodoku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sodoku
+//
+//  Created by Nathaniel Tucker on 10/13/19.
+//  Copyright © 2019 Nathaniel Tucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
